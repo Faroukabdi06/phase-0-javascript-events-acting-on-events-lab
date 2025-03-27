0fbc4a1f2b3c75117cd5063da4a346f93cc7ce03
Farouk Abdi <abdifarouk58@gmail.com>
2025-03-27 23:37:38 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+let dodger = document.querySelector('#dodger');
+let game = document.querySelector('#game');
+console.log(game);
+  document.addEventListener('keydown',  (event) =>{
+    // console.log(event);
+    if(event.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+    else if(event.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+
+  })
+
+function moveDodgerLeft(){
+
+        dodger.style.backgroundColor = "red"
+     let left1 = dodger.style.left.replace ("px", "");
+     let left2 = parseInt(left1, 10);
+     if(left2 > 0){
+     dodger.style.left = `${left2 - 1}px`
+    }
+}
+
+function moveDodgerRight (){
+    dodger.style.backgroundColor = "red"
+    let left1 = dodger.style.left.replace ("px", "");
+    let left2 = parseInt(left1, 10);
+    let dodgerWidth = dodger.offsetWidth;
+    let gameWidth =  dodger.parentElement.clientWidth;
+    dodger.style.left = `${left2 + 1}px`
+
+}
